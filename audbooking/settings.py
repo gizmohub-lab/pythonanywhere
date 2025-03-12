3250912906aef3976263303af3454f6da6b58a1c
gizmohub-lab <gizmo.hub.in@gmail.com>
2025-03-12 10:14:03 +0530

    Update settings.py

--- audbooking/settings.py
+++ audbooking/settings.py
@@ -118,6 +118,10 @@ LOGIN_URL ='login'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
